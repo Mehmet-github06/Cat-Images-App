e69909302136736320ae29d0f4fda1feaf11eb5c
mehmet dogan <mehmetseymen91@gmail.com>
2023-11-17 15:26:44 +0300

    fnish

--- app2.js
+++ app2.js
@@ -4,13 +4,6 @@ const loadingDiv = document.getElementById("loading");
 const btn = document.querySelector(".btn");
 const tarih = document.getElementById("tarih");
 
-setTimeout(() => {
-  loadingDiv.style.display = "none";
-}, 3000);
-
-let status = 0;
-let veri = "";
-
 const getImage = () => {
   fetch("https://api.thecatapi.com/v1/images/search?limit=10")
     .then((res) => {
@@ -19,18 +12,15 @@ const getImage = () => {
       }
       return res.json();
     })
-    .then((data) => {
-      veri = data;
-      console.log(veri);
-      show(data);
-    })
+    .then((data) =>  show(data))
     .catch((err) => (containerDiv.innerHTML = `<img src="./img/error.gif"/>`));
 
-  const show = (catsShow) => {
+};
+const show = (catsShow) => {
     cardDiv.innerHTML = "";
     catsShow.forEach((image) => {
       cardDiv.innerHTML += `
-            <div class="col-12 col-sm-6  col col-lg-4">
+            <div class="col-12 col-sm-6  col col-lg-4 border border-2 rounded-3">
                 <div style="height:200px;">
                     <img src="${image.url}" class="w-100 h-100" alt="...">
                 </div>
@@ -38,30 +28,31 @@ const getImage = () => {
             `;
     });
   };
-};
+
 
 btn.addEventListener("click", () => {
   setTimeout(() => {
-    cardDiv.innerHTML = `<img src="./img/loading.gif"/>`;
-  }, 1000);
+   cardDiv.innerHTML = `<img src="./img/loading.gif"/>`;
+    
+  }, 0);
+  
   getImage();
   loadingDiv.style.display = 'none';
 });
 getImage();
 
 const outputDate = () => {
-  let currentDate = new Date();
-  let datedate = currentDate.toLocaleDateString();
-  let time = currentDate.toLocaleTimeString();
+  let datedate = new Date().toLocaleDateString();
+  let time = new Date().toLocaleTimeString();
   let currentDateTimeString = datedate + " <=> " + time;
   tarih.textContent = currentDateTimeString;
 };
-setInterval(() => {
-  outputDate();
-}, 1000);
+
+setInterval(outputDate, 1000);
 
 containerDiv.style.display = "none";
 setTimeout(() => {
   loadingDiv.style.display = "none";
   containerDiv.style.display = "block";
 }, 2000);
+
